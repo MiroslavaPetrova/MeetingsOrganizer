1d437e5c2036182dc3abbd05f295e1c660d1bd85
Mimster <mn.petrova@yahoo.com>
2020-04-18 17:21:18 +0300

    Initial database created & seeded with EF code-first migrations, some friend model constraints with data annotations.

--- MeetingsOrganizer.Models/Friend.cs
+++ MeetingsOrganizer.Models/Friend.cs
@@ -1,13 +1,19 @@
-﻿namespace MeetingsOrganizer.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetingsOrganizer.Models
 {
     public class Friend
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
 
+        [StringLength(50)]
         public string LastName { get; set; }
 
+        [StringLength(50)]
         public string Email { get; set; }
     }
 }
